3d1fc00f419d2394a851d668502773a0b3dc876b
gubaojian <gubaojian@163.com>
2025-09-02 16:49:51 +0800

    增加说明文档

--- misc/gen_cert.sh
+++ misc/gen_cert.sh
@@ -1,6 +1,9 @@
 ## https://github.com/FiloSottile/mkcert
 
+## please first install ca to system
+
 mkcert -install
+
 mkcert example.com "*.example.com" example.test localhost 127.0.0.1 ::1
 
 ## https://127.0.0.1:3000/hello
