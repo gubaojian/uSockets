6d8e02974c62394ccf7c12b13ee76470c889234c
baojian.gu <gubaojian@163.com>
2025-09-02 23:03:07 +0800

    add const variable

--- src/crypto/openssl.c
+++ src/crypto/openssl.c
@@ -262,7 +262,7 @@ struct us_internal_ssl_socket_t *ssl_on_data(struct us_internal_ssl_socket_t *s,
     BIO_clear_flags(loop_ssl_data->shared_rbio,
            BIO_FLAGS_SHOULD_RETRY | BIO_FLAGS_READ | BIO_FLAGS_WRITE);
     //实际最多尝试两次，因为读缓冲区足够大，4次足够
-    int maxRetryCount = 4;
+    const int maxRetryCount = 4;
     int retry = 0;
     while (retry < maxRetryCount) {
         clearOpenSSLError();
